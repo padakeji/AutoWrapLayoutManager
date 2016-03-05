af54fde216e281c1ba21b80d0fd89a717f913277
atanl <linyongtan@126.com>
2016-03-05 16:51:48 +0800

    Change the example module fixing scroll problem.

--- example/src/main/java/com/padakeji/android/ui/autowraplayoutmanager/example/MyScrollView.java
+++ example/src/main/java/com/padakeji/android/ui/autowraplayoutmanager/example/MyScrollView.java
@@ -3,12 +3,19 @@ package com.padakeji.android.ui.autowraplayoutmanager.example;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
+import android.view.ViewConfiguration;
 import android.widget.ScrollView;
 
 /**
- * Created by atanl on 2016/3/5.
+ * 继承于ScrollView，禁止了滑动事件向下传递。
+ *
+ * Created by atanl on 2015/10/20.
  */
 public class MyScrollView extends ScrollView {
+
+    public interface OnScrollListener {
+        void onScroll();
+    }
 
     private int downX;
     private int downY;
@@ -16,14 +23,22 @@ public class MyScrollView extends ScrollView {
 
     public MyScrollView(Context context) {
         super(context);
+        mTouchSlop = ViewConfiguration.get(context).getScaledTouchSlop();
     }
 
     public MyScrollView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        mTouchSlop = ViewConfiguration.get(context).getScaledTouchSlop();
     }
 
     public MyScrollView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        mTouchSlop = ViewConfiguration.get(context).getScaledTouchSlop();
+    }
+
+    @Override
+    protected void onScrollChanged(int l, int t, int oldl, int oldt) {
+        super.onScrollChanged(l, t, oldl, oldt);
     }
 
     @Override
